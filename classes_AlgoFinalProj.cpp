e5d5f9ba8a26176e53717adf1fe9a5f369f14bdb
JWalt24293 <jwalton24293@gmail.com>
2024-10-27 17:38:04 -0400

    Continue to Implement classes

--- classes_AlgoFinalProj.cpp
+++ classes_AlgoFinalProj.cpp
@@ -13,9 +13,9 @@ struct edge{
 
 class City{
 private:
-    string name;
     vector<edge> edges = {};
 public:
+    string name;
     void add_edge(int end, int time){
         edge* New = new edge;
         New->end = end;
@@ -29,17 +29,26 @@ class map{
         vector<City> cities;
         int city_count;
     public:
+        map(){
+            city_count = 0;
+        }
         void add_city(string name){
             City* New = new City;
-            
+            New->name = name;
+            cities.push_back(*New);
+            city_count+=1;
         }
-
-
+        void add_edge(int start, int end, int time){
+            cities[start].add_edge(end, time);
+            cities[end].add_edge(start, time);
+        }
 };
 
 
 int main(){
-
+    map USA;
+    USA.add_city("Boston");
+    USA.add_city("Chicago");
 
 
 
