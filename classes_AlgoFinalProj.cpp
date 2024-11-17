db85250fdd554bb07e88e1c3a7ca6ebd029de71a
JWalt24293 <jwalton24293@gmail.com>
2024-11-17 18:34:06 -0500

    Changed Dijk to record shortest path

--- classes_AlgoFinalProj.cpp
+++ classes_AlgoFinalProj.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -12,10 +13,9 @@ struct edge {
     int end;
     int Basetime;
     int time;
-    int variation = 4;
-
+    int variation;
     void ChangeTraffic() {
-        time = Basetime + rand() % variation + 1;
+        time = Basetime + (rand() % variation) + 1;
     }
 };
 
@@ -23,10 +23,11 @@ class City {
 public:
     vector<edge> edges = {};
     string name;
-    void add_edge(int end, int time) {
+    void add_edge(int end, int time, int variation) {
         edge New;
         New.end = end;
         New.Basetime = time;
+        New.variation = variation;
         New.ChangeTraffic();
         edges.push_back(New);
     }
@@ -51,9 +52,9 @@ public:
         city_count += 1;
     }
 
-    void add_edge(string start, string end, int time) {
-        cities[cities_dictionary[start]].add_edge(cities_dictionary[end], time);
-        cities[cities_dictionary[end]].add_edge(cities_dictionary[start], time);
+    void add_edge(string start, string end, int time, int variation) {
+        cities[cities_dictionary[start]].add_edge(cities_dictionary[end], time, variation);
+        cities[cities_dictionary[end]].add_edge(cities_dictionary[start], time, variation);
     }
 
     int findMin(const vector<int>& finalized, const vector<int>& times) {
@@ -68,9 +69,11 @@ public:
         return minIndex;
     }
 
-    void dijk(int source) {
+    vector<int> dijk(int source, int destination) {
         vector<int> finalized(city_count, 0);
         vector<int> times(city_count, INT_MAX);
+        vector<int> parent(city_count, -1);
+
         times[source] = 0;
 
         int current;
@@ -80,15 +83,25 @@ public:
             finalized[current] = 1;
 
             for (size_t x = 0; x < cities[current].edges.size(); x++) {
-                if (!finalized[cities[current].edges[x].end]) {
-                    times[cities[current].edges[x].end] = min(times[cities[current].edges[x].end], times[current] + cities[current].edges[x].time);
+                if (!finalized[cities[current].edges[x].end] && times[cities[current].edges[x].end] > times[current] + cities[current].edges[x].time) {
+                    times[cities[current].edges[x].end] = times[current] + cities[current].edges[x].time;
+                    parent[cities[current].edges[x].end] = current;
                 }
             }
         }
 
-        for (int i = 0; i < city_count; i++)
+        for (int i = 0; i < city_count; i++) {
             cout << i << " \t\t\t\t" << times[i] << endl;
+        }
+        vector<int> path;
+        for (int postition = destination; postition != -1; postition = parent[postition] ) {
+            path.push_back(postition);
+        }
+        reverse(path.begin(), path.end());
+        return path;
     }
+
+
 };
 
 int main() {
@@ -100,12 +113,18 @@ int main() {
     USA.add_city("Keene");    // 3
     USA.add_city("Cambridge");// 4
 
-    USA.add_edge("Boston", "Chicago", 15);
-    USA.add_edge("Chicago", "Portland", 8);
-    USA.add_edge("Boston", "Portland", 4);
-    USA.add_edge("Chicago", "Keene", 10);
-    USA.add_edge("Cambridge", "Portland", 3);
-    USA.dijk(0);
+    USA.add_edge("Boston", "Chicago", 30, 8);
+    USA.add_edge("Boston", "Chicago", 40, 1);
+    USA.add_edge("Chicago", "Portland", 28,1);
+    USA.add_edge("Boston", "Portland", 4,1);
+    USA.add_edge("Chicago", "Keene", 10,1);
+    USA.add_edge("Cambridge", "Portland", 3,1);
+    vector<int> record = USA.dijk(0,3);
+
+    for (size_t i = 0; i < record.size(); i++) {
+        cout << record[i];
+        if (i < record.size() - 1) cout << " -> ";
+    }
 
     return 0;
 }
