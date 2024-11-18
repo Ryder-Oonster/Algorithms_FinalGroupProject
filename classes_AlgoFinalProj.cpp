63da0135dde56cee421d2d1b2c28166ba6116eff
JWalt24293 <jwalton24293@gmail.com>
2024-11-17 20:24:38 -0500

    Combined Branches

--- classes_AlgoFinalProj.cpp
+++ classes_AlgoFinalProj.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -5,18 +6,30 @@
 #include <climits> // For INT_MAX
 #include <cstdlib> // For rand()
 #include <ctime>   // For srand()
-#include <fstream>
+#include <math.h>
 
 using namespace std;
+double hour;
+double trafficCalc() {
+    if (hour >=5.5 && hour <=10.5) {
+     return (0.277778*(pow(hour, 3))-10.2976*(pow(hour,2))+108.948*hour-325.238);
+    }
+    else if (hour >= 12.5 && hour <=20.5) {
+        return 0.353535*(pow(hour,3))-23.5119*(pow(hour,2))+482.682*hour-3045.95;
+    }
+    else {
+        return 0;
+    }
+}
+
 
 struct edge {
     int end;
     int Basetime;
     int time;
-    int variation = 4;
-
+    int variation;
     void ChangeTraffic() {
-        time = Basetime + rand() % variation + 1;
+        time = Basetime + (1+trafficCalc()*variation*1);
     }
 };
 
@@ -24,10 +37,11 @@ class City {
 public:
     vector<edge> edges = {};
     string name;
-    void add_edge(int end, int time) {
+    void add_edge(int end, int time, int variation) {
         edge New;
         New.end = end;
         New.Basetime = time;
+        New.variation = variation;
         New.ChangeTraffic();
         edges.push_back(New);
     }
@@ -52,9 +66,9 @@ public:
         city_count += 1;
     }
 
-    void add_edge(string start, string end, int time) {
-        cities[cities_dictionary[start]].add_edge(cities_dictionary[end], time);
-        cities[cities_dictionary[end]].add_edge(cities_dictionary[start], time);
+    void add_edge(string start, string end, int time, int variation) {
+        cities[cities_dictionary[start]].add_edge(cities_dictionary[end], time, variation);
+        cities[cities_dictionary[end]].add_edge(cities_dictionary[start], time, variation);
     }
 
     int findMin(const vector<int>& finalized, const vector<int>& times) {
@@ -69,9 +83,11 @@ public:
         return minIndex;
     }
 
-    void dijk(int source) {
+    vector<int> dijk(int source, int destination) {
         vector<int> finalized(city_count, 0);
         vector<int> times(city_count, INT_MAX);
+        vector<int> parent(city_count, -1);
+
         times[source] = 0;
 
         int current;
@@ -81,21 +97,30 @@ public:
             finalized[current] = 1;
 
             for (size_t x = 0; x < cities[current].edges.size(); x++) {
-                if (!finalized[cities[current].edges[x].end]) {
-                    times[cities[current].edges[x].end] = min(times[cities[current].edges[x].end], times[current] + cities[current].edges[x].time);
+                if (!finalized[cities[current].edges[x].end] && times[cities[current].edges[x].end] > times[current] + cities[current].edges[x].time) {
+                    times[cities[current].edges[x].end] = times[current] + cities[current].edges[x].time;
+                    parent[cities[current].edges[x].end] = current;
                 }
             }
         }
 
-        for (int i = 0; i < city_count; i++)
+        for (int i = 0; i < city_count; i++) {
             cout << i << " \t\t\t\t" << times[i] << endl;
+        }
+        vector<int> path;
+        for (int postition = destination; postition != -1; postition = parent[postition] ) {
+            path.push_back(postition);
+        }
+        reverse(path.begin(), path.end());
+        return path;
     }
+
+
 };
 
 int main() {
     srand(static_cast<unsigned int>(time(0)));
     World USA;
-
     USA.add_city("Boston");
     USA.add_city("Salem");
     USA.add_city("Burlington");
@@ -148,8 +173,14 @@ int main() {
     USA.add_edge("Orange","Springfield",1);
     USA.add_edge("Greenfield","Springfield",3);
     USA.add_edge("Worcester","Springfield",3);
+    vector<int> record = USA.dijk(0,3);
 
-    USA.dijk(0);
+    for (size_t i = 0; i < record.size(); i++) {
+        cout << record[i];
+        if (i < record.size() - 1) cout << " -> ";
+    }
 
+    hour = 11;
+    cout<<endl<<trafficCalc();
     return 0;
 }
